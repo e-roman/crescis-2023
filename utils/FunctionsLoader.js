c2a234b9f05ff276560e8985b7edd32d76259a1f
e-roman~ <emiliano.roman@gmail.com>
2025-04-29 22:55:50 -0300

    Fixs loader10

--- utils/FunctionsLoader.js
+++ utils/FunctionsLoader.js
@@ -14,6 +14,8 @@ export function initPageTransition() {
     tl.to(".ptr-overlay",         { scaleY: 1, transformOrigin: "center bottom" }, 0);
     tl.to("#content-wrap",        { y: -80, autoAlpha: 0 }, 0);
     tl.to("#tt-header",           { y: -20, autoAlpha: 0 }, 0);
+    tl.to(".ph-image",           { y: -80, autoAlpha: 0 }, 0);
+    tl.to(".ph-appear",           { y: -60, autoAlpha: 0 }, 0);
     tl.to(".ptr-preloader",       { autoAlpha: 1 }, 0.4);
   }
 
@@ -22,9 +24,9 @@ export function initPageTransition() {
     tl.to(".ptr-preloader", { duration: 1, autoAlpha: 0, ease: Expo.easeInOut });
     tl.to(".ptr-overlay",   { duration: 1, scaleY: 0, transformOrigin: "center top", ease: Expo.easeInOut }, 0.3);
     tl.from("#tt-header",   { duration: 1, y: 20, autoAlpha: 0, ease: Expo.easeInOut, clearProps: "all" }, 0.6);
-    tl.from(".ph-image", { duration: 1.5, y: 80, autoAlpha: 0, stagger: 0.3, ease: Expo.easeOut, clearProps:"all" }, 1.2);
-    tl.from(".ph-appear", { duration: 1.5, y: 60, autoAlpha: 0, stagger: 0.3, ease: Expo.easeOut, clearProps:"all" }, 1.5);
-    // ... aquí tus demás from() de ph-image, ph-appear, etc.
+    tl.from(".ph-image",    { duration: 1.5, y: 80, autoAlpha: 0, stagger: 0.3, ease: Expo.easeOut, clearProps:"all" }, 1.2);
+    tl.from(".ph-appear",   { duration: 1.5, y: 60, autoAlpha: 0, stagger: 0.3, ease: Expo.easeOut, clearProps:"all" }, 1.5);
+
     tl.set("#page-transition", { duration: 1, autoAlpha: 0, ease: Expo.easeInOut });
   }
 
